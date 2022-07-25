41817792196ce32416c116dbab91f06b584b1435
Victoriia-Ripka <vikaripka@gmail.com>
2022-07-25 21:27:38 +0200

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+const counter = {
+    value: 0,
+    increment() {
+        this.value += 1;
+    },
+    decrement() {
+        this.value -= 1;
+    },
+};
+const counterValue = document.querySelector('#value');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+incrementBtn.addEventListener('click', () => {
+    counter.increment();
+    counterValue.textContent = counter.value;
+});
+
+decrementBtn.addEventListener('click', () => {
+    counter.decrement();
+    counterValue.textContent = counter.value;
+});

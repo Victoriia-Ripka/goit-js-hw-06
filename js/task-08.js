58e8394ec28c9394076c4497f107d861bd88c085
Victoriia-Ripka <vikaripka@gmail.com>
2022-07-27 08:55:44 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,29 +1,16 @@
 const form = document.querySelector('.login-form');
-const btn = document.querySelector('[type="submit"]');
-
-function send(event) {
-    const formElements = event.currentTarget.elements;
-        const email = formElements.email.value;
-        const password = formElements.password.value;
-        const formData = {
-            email,
-            password,
-        }
-        console.log(formData);
-}
+form.addEventListener('submit', doLogin);
 
 function doLogin (event) {
     event.preventDefault();
-    if (!event.currentTarget.elements.email.value || !event.currentTarget.elements.password.value) {
-        console.log('alert');
-        //таск вісім не выдправляэмо не до кынця заповнену форму, якщо пароль або мейл порожні то показуємо аларм
-        //53:00 second lesson change some tags due to other tags 
-        btn.disabled = !event.currentTarget.elements.email.value || !event.currentTarget.elements.password.value;
+    const {
+        elements: {email,password},
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        alert('alert');
     } else {
-        btn.abled = event.currentTarget.elements.email.value && event.currentTarget.elements.password.value;
-        form.addEventListener('submit', send);
-        //Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+        console.log(`Login: ${email.value}`);
+    console.log(`Password: ${password.value}`);
+    event.currentTarget.reset();
     }
 }
-
-form.addEventListener('change', doLogin);
